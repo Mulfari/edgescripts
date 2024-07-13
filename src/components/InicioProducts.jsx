f7d3f3863d5ccd651d584b418d66e444fc12ac07
Mulfari <josesalvador0102@gmail.com>
2024-07-13 20:02:28 +0100

    product inicio

--- src/components/InicioProducts.jsx
+++ src/components/InicioProducts.jsx
@@ -26,11 +26,11 @@ const InicioProducts = () => {
   return (
     <div
       id="inicioProducts"
-      className="relative w-screen h-[15.6rem] bg-fixed bg-center bg-cover overflow-hidden"
+      className="relative w-screen h-[17.16rem] bg-fixed bg-center bg-cover overflow-hidden"
       style={{ backgroundImage: `url(${i1})` }}
     >
       <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black to-transparent opacity-50"></div>
-      <div className="relative z-10 flex items-center justify-center h-full text-center text-white">
+      <div className="relative z-10 flex items-center justify-start h-full text-center text-white pl-10">
         <h1 className="text-4xl font-bold" style={{ fontFamily: '"Press Start 2P", cursive' }}>Products</h1>
       </div>
     </div>
