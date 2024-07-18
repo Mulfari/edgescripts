60b157555139caecd5e972b5930482ec71fe6f3e
Mulfari <josesalvador0102@gmail.com>
2024-07-18 20:34:25 +0100

    up

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -12,10 +12,10 @@ const Inicio = () => {
         <div className="grid lg:grid-cols-12 lg:gap-8 xl:gap-0">
           <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">
-              Payments toolsdfg for software companies
+              Welcome to EdgeScripts
             </h1>
             <p className="max-w-2xl mb-6 font-light text-gray-200 lg:mb-8 md:text-lg lg:text-xl">
-              From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.
+            your ultimate source for top-tier gaming scripts designed for online games. Elevate your gameplay with our cutting-edge solutions.
             </p>
             <a 
               href="#" 
