0fcbcc988362c8847c82891e9d08da3d601a72f0
Mulfari <josesalvador0102@gmail.com>
2024-07-18 13:39:32 +0100

    up

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,21 +1,22 @@
 // src/components/Inicio.jsx
 
 import React from 'react';
+import s1 from '../assets/s1.png'; // Asegúrate de que la ruta sea correcta
 
 const Inicio = () => {
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-gradient-to-r from-gray-700 via-gray-900 to-black dark:bg-gray-900">
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
-          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">
             Payments tool for software companies
           </h1>
-          <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+          <p className="max-w-2xl mb-6 font-light text-gray-300 lg:mb-8 md:text-lg lg:text-xl">
             From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.
           </p>
           <a 
             href="#" 
-            className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+            className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
           >
             Get started
             <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -30,7 +31,7 @@ const Inicio = () => {
           </a> 
         </div>
         <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-          <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup" />
+          <img src={s1} alt="mockup" />
         </div>                
       </div>
     </section>
