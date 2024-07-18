b9e9a250b3274162d89ed879ddea80fe0e41fb3d
Mulfari <josesalvador0102@gmail.com>
2024-07-18 13:22:16 +0100

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AuthProvider from './AuthContext';
-import Inicio from './components/Inicio';
+import HeroSection from './components/HeroSection';
 import InicioProducts from './components/InicioProducts';
 import Header from './components/Header';
 import Footer from './components/Footer';
