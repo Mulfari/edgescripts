fa81de70e57b35cf7c02e8f841d46f737f4056f8
Mulfari <josesalvador0102@gmail.com>
2024-07-18 13:07:41 +0100

    up

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -4,7 +4,6 @@ import i1 from '../assets/i1.jpg';
 import s1 from '../assets/s1.png';
 
 const Inicio = () => {
-  const [animateClass, setAnimateClass] = useState('');
   const [isReduced, setIsReduced] = useState(false);
 
   useEffect(() => {
@@ -28,19 +27,19 @@ const Inicio = () => {
 
   return (
     <div
-      className="relative w-screen h-[39rem] md:h-[19.5rem] lg:h-[39rem] bg-fixed bg-center bg-cover overflow-hidden"
+      className="relative w-full h-[39rem] md:h-[19.5rem] lg:h-[39rem] bg-fixed bg-center bg-cover overflow-hidden"
       style={{ backgroundImage: `url(${i1})` }}
     >
       <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black to-transparent opacity-50"></div>
-      <div className={`relative z-10 flex flex-col items-start justify-center h-full text-left text-white px-40 md:px-56 lg:px-72 ${isReduced ? 'animate-moveLeft' : 'animate-moveRight'}`} style={{ maxWidth: '70%' }}>
-        <div className="mt-16 mb-6 lg:block">
-          <p className="text-lg md:text-xl lg:text-2xl leading-relaxed" style={{ fontFamily: 'Play, sans-serif' }}>
+      <div className={`relative z-10 flex flex-col items-start justify-center h-full text-left text-white p-6 md:px-20 lg:px-32 ${isReduced ? 'animate-moveLeft' : 'animate-moveRight'}`} style={{ maxWidth: '70%' }}>
+        <div className="mt-16 mb-6">
+          <p className="text-base md:text-lg lg:text-xl leading-relaxed font-play">
             Welcome to EdgeScripts, your ultimate source for top-tier gaming scripts designed for online games. Elevate your gameplay with our cutting-edge solutions.
           </p>
         </div>
         <div className="mt-6 md:mt-8 lg:mt-10">
           <div style={{ height: '3rem', width: '100%' }}>
-            <h1 className="text-lg md:text-xl lg:text-2xl font-bold mb-4" style={{ whiteSpace: 'nowrap', overflow: 'visible', textOverflow: 'clip', fontFamily: '"Press Start 2P", cursive', color: '#3B82F6' }}>
+            <h1 className="text-base md:text-lg lg:text-xl font-bold mb-4 whitespace-nowrap overflow-visible text-clip font-press-start text-blue-500">
               <Typewriter
                 words={['Unlock your gaming potential']}
                 loop={0}
@@ -58,8 +57,8 @@ const Inicio = () => {
         id="characterImage"
         src={s1}
         alt="Character"
-        className="hidden xl:block absolute bottom-10 right-32 md:right-48 lg:right-64 z-20 animate-fadeInUp image-responsive"
-        style={{ width: '600px', height: 'auto' }}
+        className="hidden xl:block absolute bottom-10 right-8 md:right-20 lg:right-32 z-20 animate-fadeInUp"
+        style={{ width: '30vw', height: 'auto', maxWidth: '600px' }}
       />
     </div>
   );
