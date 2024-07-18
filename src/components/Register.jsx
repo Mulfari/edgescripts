df4d269870ff8ddacfa523fc6e117f80f7c9ccb3
Mulfari <josesalvador0102@gmail.com>
2024-07-18 19:59:35 +0100

    up

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,13 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FaEye, FaEyeSlash, FaCheckCircle } from 'react-icons/fa';
 import zxcvbn from 'zxcvbn';
-import './Spinner.css'; // Importar el archivo CSS para el spinner
 
 const isValidEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 const isValidPassword = (password) => password.length >= 8 && password.length <= 32;
 const getPasswordStrength = (password) => zxcvbn(password).score;
-
-const Spinner = () => <div className="spinner"></div>; // Nuevo componente de spinner
+const Spinner = () => <div className="loader">Loading...</div>;
 
 const Register = () => {
   const [email, setEmail] = useState('');
