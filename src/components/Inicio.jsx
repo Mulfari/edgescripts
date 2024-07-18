a2859846d917d7c414b8882523e98b868ee8564a
Mulfari <josesalvador0102@gmail.com>
2024-07-18 19:14:09 +0100

    up

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -12,7 +12,7 @@ const Inicio = () => {
         <div className="grid lg:grid-cols-12 lg:gap-8 xl:gap-0">
           <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">
-              Payments tool for software companies
+              Payments toolsdfg for software companies
             </h1>
             <p className="max-w-2xl mb-6 font-light text-gray-200 lg:mb-8 md:text-lg lg:text-xl">
               From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.
